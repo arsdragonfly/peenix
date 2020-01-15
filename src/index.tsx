878e0251cd9bb64dca73ed4f3769e8b1dcf96f06
arsdragonfly <arsdragonfly@gmail.com>
2020-01-15 00:04:16 -0500

    change to react-hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ReduxRoot } from "./ReduxRoot";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement);
-registerServiceWorker();
+const rootEl = document.getElementById("root");
+ReactDOM.render(<ReduxRoot />, rootEl);
+
+// comment in for PWA with service worker in production mode
+// registerServiceWorker();
