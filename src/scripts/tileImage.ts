3a15b78ed7b9be2d4d3e86e56bef300f2cd90f25
arsdragonfly <arsdragonfly@gmail.com>
2020-07-01 15:09:59 +0800

    add missing nan & import PNGs

---
+++ src/scripts/tileImage.ts
@@ -0,0 +1,48 @@
+// Characters
+import t1m from "../../tile_images/Man1.png"
+import t2m from "../../tile_images/Man2.png"
+import t3m from "../../tile_images/Man3.png"
+import t4m from "../../tile_images/Man4.png"
+import t5m from "../../tile_images/Man5.png"
+import t6m from "../../tile_images/Man6.png"
+import t7m from "../../tile_images/Man7.png"
+import t8m from "../../tile_images/Man8.png"
+import t9m from "../../tile_images/Man9.png"
+
+// Circles
+import t1p from "../../tile_images/Pin1.png"
+import t2p from "../../tile_images/Pin2.png"
+import t3p from "../../tile_images/Pin3.png"
+import t4p from "../../tile_images/Pin4.png"
+import t5p from "../../tile_images/Pin5.png"
+import t6p from "../../tile_images/Pin6.png"
+import t7p from "../../tile_images/Pin7.png"
+import t8p from "../../tile_images/Pin8.png"
+import t9p from "../../tile_images/Pin9.png"
+
+// Bamboos
+import t1s from "../../tile_images/Sou1.png"
+import t2s from "../../tile_images/Sou2.png"
+import t3s from "../../tile_images/Sou3.png"
+import t4s from "../../tile_images/Sou4.png"
+import t5s from "../../tile_images/Sou5.png"
+import t6s from "../../tile_images/Sou6.png"
+import t7s from "../../tile_images/Sou7.png"
+import t8s from "../../tile_images/Sou8.png"
+import t9s from "../../tile_images/Sou9.png"
+
+// Wind
+import t1z from "../../tile_images/Ton.png"
+import t2z from "../../tile_images/Nan.png"
+import t3z from "../../tile_images/Shaa.png"
+import t4z from "../../tile_images/Pei.png"
+
+// Dragon
+import t5z from "../../tile_images/Haku.png"
+import t6z from "../../tile_images/Hatsu.png"
+import t7z from "../../tile_images/Chun.png"
+
+// Red Fives
+import t0m from "../../tile_images/Man5-Dora.png"
+import t0p from "../../tile_images/Pin5-Dora.png"
+import t0s from "../../tile_images/Sou5-Dora.png"
