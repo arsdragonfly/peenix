189cc246c250ab37310af81dac564e8e5a85464c
Zheyu Shen <arsdragonfly@gmail.com>
2021-10-04 09:47:53 +0800

    add new mui packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
 import { AppBar, Divider, Drawer as DrawerMui, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography, useMediaQuery } from '@material-ui/core'
-import { Theme } from "@material-ui/core/styles";
+import { Theme, makeStyles } from "@material-ui/core/styles";
 import HomeIcon from "@material-ui/icons/Home";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles } from "@material-ui/styles";
 import * as React from "react";
 import { Route, Router } from "react-router-dom";
 import { history } from "./store";
 import { WWYDPage } from "./pages";
 import { withRoot } from "./withRoot";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+    zIndex: 1,
+    overflow: "hidden"
+  },
+  appFrame: {
+    position: "relative",
+    display: "flex",
+    width: "100%",
+    height: "100%"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    position: "absolute"
+  },
+  navIconHide: {
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  },
+  drawerHeader: theme.mixins.toolbar,
+  drawerPaper: {
+    width: 250,
+    backgroundColor: theme.palette.background.default,
+    [theme.breakpoints.up("md")]: {
+      width: drawerWidth,
+      position: "relative",
+      height: "100%"
+    }
+  },
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: "100%",
+    height: "calc(100% - 56px)",
+    marginTop: 56,
+    [theme.breakpoints.up("sm")]: {
+      height: "calc(100% - 64px)",
+      marginTop: 64
+    }
+  }
+}));
 
 const Routes: React.FC = () => {
   const classes = useStyles();
@@ -90,50 +135,5 @@ const App: React.FC = () => {
     </Router>
   );
 };
-
-const drawerWidth = 240;
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    width: "100%",
-    height: "100%",
-    zIndex: 1,
-    overflow: "hidden"
-  },
-  appFrame: {
-    position: "relative",
-    display: "flex",
-    width: "100%",
-    height: "100%"
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    position: "absolute"
-  },
-  navIconHide: {
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  },
-  drawerHeader: theme.mixins.toolbar,
-  drawerPaper: {
-    width: 250,
-    backgroundColor: theme.palette.background.default,
-    [theme.breakpoints.up("md")]: {
-      width: drawerWidth,
-      position: "relative",
-      height: "100%"
-    }
-  },
-  content: {
-    backgroundColor: theme.palette.background.default,
-    width: "100%",
-    height: "calc(100% - 56px)",
-    marginTop: 56,
-    [theme.breakpoints.up("sm")]: {
-      height: "calc(100% - 64px)",
-      marginTop: 64
-    }
-  }
-}));
 
 export default withRoot(App);
